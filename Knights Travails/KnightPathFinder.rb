3934fc45877f9bb5407f71aaad80484caea4b5a1
wliu <wliu0709@gmail.com>
2021-05-10 13:31:35 -0400

    first modification

--- Knights Travails/KnightPathFinder.rb
+++ Knights Travails/KnightPathFinder.rb
@@ -2,11 +2,11 @@ require 'byebug'
 require_relative '00_tree_node'
 require_relative 'knightstravails'
 require_relative 'board'
+require 'set'
 class KnightPathFinder
 
     def self.valid_moves(pos)# array of pos[0,0] row and col
         #[2,1],[1,2],[-1,-2],[-2,-1],[2,-1],[1,-2],[-1,2],[-2,1]
-        @considered_positions = pos
         row,col = pos
         real_pos = [[row + 2, col + 1], [row + 1, col + 2], [row - 1, col -2], 
             [row - 2, col - 1], [row + 2, col - 1], [row + 1, col - 2], [row - 1, col + 2], [row - 2, col + 1]]
@@ -19,18 +19,32 @@ class KnightPathFinder
 
     def initialize(start_pos)
         @start_pos = start_pos
-        # @board = Board.new
+        @considered_positions = [start_pos]
+        build_move_tree
     end
 
     def build_move_tree
         self.root_node = PolyTreeNode.new(start_pos)
+        nodes = [root_node] # node instance  
+        i = 0
+        while (i < nodes.length)
+            ele = nodes.shift
+            pos = new_move_positions(ele.value)
+            #######
+            current_node = PolyTreeNode.new(ele)
+            pos.each do |po|
+                 node = PolyTreeNode.new(po)
+                 node.parent = current_node
+                 nodes << node
+            end 
+        end 
     end
 
     def new_move_positions(pos)
         new_pos = KnightPathFinder.valid_moves(pos).select{|ele|!considered_positions.include?(ele)}
-        considered_positions += new_pos
-        new_pos
+        @considered_positions += new_pos
     end
-    private
-    attr_reader :start_pos, :considered_positions
+    # private 
+    attr_reader :root_node, :considered_positions, :start_pos
+    attr_writer :root_node
 end
